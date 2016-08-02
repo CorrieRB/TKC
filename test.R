4ed180036b29f4f380a0f11d23bc3b73486d0ce5
Corrie Belanger <corrie@cmdr.hancock.com>
2016-08-02 10:33:58 -0700

    edited pushing ro remote

--- test.R
+++ test.R
@@ -1,1 +1,3 @@
-A second file
+# A second file with some notes for git
+#change the script and then commit on the right hand side
+#to add to my online repository have to use command line to push origin master
